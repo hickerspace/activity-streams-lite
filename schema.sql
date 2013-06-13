96e2b4c5f27509cd01173891f91874d7499362a3
basti2342 <basti@randomprojects.de>
2013-06-14 01:17:13 +0200

    utf8, unique changed

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE activities (
 	pk_id SERIAL PRIMARY KEY,
 	datetime DATETIME NOT NULL,
-	person TEXT NOT NULL,
-	service TEXT NOT NULL,
-	type TEXT NOT NULL,
-	content TEXT NOT NULL,
-	url TEXT NOT NULL,
+	person TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	service TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	type TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	content TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	url TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
 	UNIQUE (
 		datetime,
-		person(198),
-		service(198),
-		type (198),
-		content(198),
-		url(198)
+		person(30),
+		service(30),
+		type (30),
+		content(165),
+		url(75)
 	)
 );
